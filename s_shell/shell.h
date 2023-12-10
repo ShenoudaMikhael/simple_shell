83465f2ffe8cd0ef6fd6fe62b3d886df3d4154bf
Salsabil <salsabeelahmed323@gmail.com>
2023-12-10 20:51:48 +0200

    shell commit

---
+++ s_shell/shell.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+void execmd(char **argv);
+char *get_path(char *command);
+int main(int ac, char **argv);
